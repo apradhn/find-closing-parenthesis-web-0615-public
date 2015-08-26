2016abe292b8ba09d51f8f299c26810b45a4731f
apradhn <abhijit.pradhan6@gmail.com>
2015-08-26 12:45:47 -0400

    completes challenge

--- find_closing_parenthesis.rb
+++ find_closing_parenthesis.rb
@@ -1,3 +1,10 @@
 def find_closing_parenthesis(sentence, open_position)
-
+  chars = sentence.split("")
+  substring = chars[open_position..-1]
+  i = 1
+  substring.each do |char|
+    break if char == ")"
+    i += 1
+  end
+  i + open_position
 end
