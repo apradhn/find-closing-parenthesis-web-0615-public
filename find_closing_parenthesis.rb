b594bf4384b06fa342c0804dd0adb9a9b158bc18
apradhn <abhijit.pradhan6@gmail.com>
2015-08-29 00:10:59 -0400

    removes redundant code

--- find_closing_parenthesis.rb
+++ find_closing_parenthesis.rb
@@ -1,9 +1,6 @@
 def find_closing_parenthesis(sentence, open_position)
   substring = sentence[open_position..-1]
-  i = 1
-  substring.split("").each do |char|
-    break if char == ")"
-    i += 1
+  substring.split("").each.with_index(1) do |char, i|
+    return i + open_position if char == ")"
   end
-  i + open_position
 end
