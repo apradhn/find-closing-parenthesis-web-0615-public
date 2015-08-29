5af3e2cd049424c5532a0b08985e1c9952d86bb0
apradhn <abhijit.pradhan6@gmail.com>
2015-08-29 00:13:40 -0400

    style tweaks

--- find_closing_parenthesis.rb
+++ find_closing_parenthesis.rb
@@ -1,5 +1,3 @@
 def find_closing_parenthesis(sentence, open_position)
-  sentence[open_position..-1].split("").each.with_index(1) do |char, i|
-    return i + open_position if char == ")"
-  end
+  sentence[open_position..-1].split("").each.with_index(1) { |char, i| return i + open_position if char == ")" }
 end
